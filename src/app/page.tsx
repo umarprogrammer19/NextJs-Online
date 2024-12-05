c03e754dc520b9e273b98224a9da187e4511632d
Umar Farooq <154402323+umarprogrammer19@users.noreply.github.com>
2024-12-06 00:33:17 +0500

    Header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
+import Header from "@/components/Header";
 import Navbar from "@/components/Navbar";
 export default function page() {
   return (
     <div>
+      <Header />
       <Navbar />
     </div>
   )
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,13 +1,27 @@
 import Image from "next/image";
+
 export default function Navbar() {
     return (
         <>
-            <div className="h-12 w-screen bg-black text-white flex
-            justify-center items-center">
-                <h1>Summer Sale For All Swim Suits And Free Express Delivery - OFF 50%! <span className="underline">ShopNow</span></h1>
-                <div className="flex sm:relative sm:left-12 md:relative md:left-24 lg:relative lg:left-48">
-                    <h1>English</h1>
-                    <Image src={"/DropDown.png"} alt="Error From Getting Image" width={20} height={20}/>
+            <div className="h-[100px] border-b border-b-black flex justify-center items-center">
+                <div className="w-4/5 h-9 flex justify-between items-center">
+                    <div className="text-2xl font-bold tracking-wider">Exclusive</div>
+                    <div className="space-x-5">
+                        <a href="#" className="underline">Home</a>
+                        <a href="#">About</a>
+                        <a href="#">Contact</a>
+                        <a href="#">Sign Up</a>
+                    </div>
+                    <div className="flex items-center gap-6">
+                        <div className="flex px-4">
+                            <input type="text" placeholder="What are you looking for?" className="placeholder:text-[15px]"/>
+                            <Image src="/Search.png" alt="Wish" width={25} height={25}></Image>
+                        </div>
+                        <div className="flex items-center gap-4">
+                            <Image src="/Wishlist.png" alt="Wish" width={30} height={30}></Image>
+                            <Image src="/Cart.png" alt="Cart" width={30} height={30}></Image>
+                        </div>
+                    </div>
                 </div>
             </div>
         </>
