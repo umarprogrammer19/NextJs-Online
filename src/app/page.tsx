6239f2ec1900efe5dd4d15a8c5acffdac127fd38
Umar Farooq <154402323+umarprogrammer19@users.noreply.github.com>
2024-12-05 23:53:01 +0500

    Firsr Commit

---
+++ src/app/page.tsx
@@ -0,0 +1,8 @@
+import Navbar from "@/components/Navbar";
+export default function page() {
+  return (
+    <div>
+      <Navbar />
+    </div>
+  )
+}
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,15 @@
+import Image from "next/image";
+export default function Navbar() {
+    return (
+        <>
+            <div className="h-12 w-screen bg-black text-white flex
+            justify-center items-center">
+                <h1>Summer Sale For All Swim Suits And Free Express Delivery - OFF 50%! <span className="underline">ShopNow</span></h1>
+                <div className="flex sm:relative sm:left-12 md:relative md:left-24 lg:relative lg:left-48">
+                    <h1>English</h1>
+                    <Image src={"/DropDown.png"} alt="Error From Getting Image" width={20} height={20}/>
+                </div>
+            </div>
+        </>
+    )
+}
